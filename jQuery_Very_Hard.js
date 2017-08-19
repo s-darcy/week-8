d6fb2ddc51b87ff78a81eb02558bc30ca9edbad6
Shane <sdarcy@redventures.com>
2017-08-19 12:57:20 -0400

    very_hard 1:00pm

--- jQuery_Very_Hard.js
+++ jQuery_Very_Hard.js
@@ -18,12 +18,9 @@ $(document).ready(function(){
         
 //4. Allow the user to start the timer once those questions have been answered.    
     //checks if all inputs are complete
-    $('#startTimer').click(function(){
+    $('.startTimer').click(function(){
         if(!numberOfSets || !setLength || !restLength){
             $('#clock').html("Please fill out all fields.");
-        } else {
-            //inserts clock onto DOM
-            $('#clock').html(curClock);
         }
     }); //if/else inputs checkers
     
@@ -31,37 +28,60 @@ $(document).ready(function(){
     
         
 //5. Allow the user to stop the timer at any time after it has started.
-        $('#startTimer').click(function(){
+        $('.startTimer').click(function(){
            if($(this).html()=="Start Timer"){
-              $(this).html("Stop Timer"); 
-           } else {
+               $(this).html("Stop Timer");
+               $(this).removeClass("startTimer");
+               $(this).addClass("stopTimer");  
+           } else if ($(this).hasClass("stopTimer")){
                $(this).html("Start Timer");
-           } 
+               $(this).removeClass("stopTimer");
+               $(this).addClass("restTimer"); 
+           } else {
+               $(this).html("Stopped");
+               $(this).removeClass("restTimer");
+               $(this).addClass("stopTimer");
+           }
         });
     
 //6. Visually count down each activity period and then each rest period.
-       
-    $('#startTimer').click(function(){
+    var timer = null, 
+        interval = 1000,
+        sec = 0;   
+    $('.startTimer').click(function(){
         $(this).addClass("active");
-          var sec = 0;
+
+        if (timer !== null) return;
             function clock (val) {return val > 9 ? val : "0" + val;}
-            setInterval(function(){
+            timer = setInterval(function(){
                 $("#seconds").html(clock(++sec%60));
-                $("#minutes").html(clock(parseInt(sec/setLength, 10)));
-            }, 1000);
+                $("#counter").html(clock(parseInt(sec/setLength, 10)));
+            }, interval);
             console.log(sec);
-                if (sec === setLength){
-                    return clearInterval(clock);
-                        //call rest function to start it
-                } else if ($(this) !== "active") { 
-                       return clearInterval(clock);
-                        sec = 0;
-                } else {  
-
-                }    
+//                if (sec === setLength){
+//                    return clearInterval(clock);
+//                        //call rest function to start it
+//                } else {  
+//                    clearInterval(clock);
+//                }
     }); //start timer
-    
+        
+    function restClock () {
+        var restSec = 0;    
+        function restClock (val) {return val > 9 ? val : "0" + val;}
+        var restTimer = setInterval(function(){
+            $("#restSeconds").html(restClock(++restSec%60));
+            $("#restCounter").html(restClock(parseInt(restSec/restLength, 10)));
+        }, 1000);    
+    };
+    $('.stopTimer').click(function(){
+            clearInterval(timer);
+            console.log(restClock());
+            timer = null;
+    });
+   
 
+        
 
     //Loop to decrement
 //    function countDown () {
