29aac1b4748c4671e28c9dcfbc8858321fb01e1d
Shane <sdarcy@redventures.com>
2017-08-19 20:10:53 -0400

    8:10pm

--- jQuery_Very_Hard.js
+++ jQuery_Very_Hard.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         //converts current time into a digital clock
         var curClock = new Date().toTimeString();
     //        .replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
-
+        console.log(curClock);
 
     //4. Allow the user to start the timer once those questions have been answered.    
         //checks if all inputs are complete
@@ -23,73 +23,79 @@ $(document).ready(function(){
             }
         }); //if/else inputs checkers
 
-        console.log(curClock);
+        
 
     //5. Allow the user to stop the timer at any time after it has started.
     //6. Visually count down each activity period and then each rest period.
    
-    $('.startTimer').click(function(){
+    $('.startTimer').click(function(e){
         $(this).addClass("active");
 //        $(this).removeClass("startTimer");
 //        $(this).addClass("restTimer");  
       
-        $('#stop').click(function(){
-            restFunc();
-        });  
-        
-        //start timer
-        var timer = null, 
-            interval = 1000,
-            sec = setLength;
-
-                if (timer !== null) return;
-                function clock (val) {return val > 9 ? val : "0" + val;}
-                timer = setInterval(function(){
-                    $("#seconds").html(clock(--sec%60));
-                    $("#counter").html(sets[x]);  
-                     if(sec === 0){
-                        clearInterval(timer);
-                        restFunc();
-                        $('#seconds').html('--');
-                     } else {
-                        $('#stop').click(function(){
-                            clearInterval(timer);
-                            $('#seconds').html('--');
-                        });   
-                     } //if
-                 }, interval);
-                //timer
-                console.log(sec);
-                
-            //rest timer
-            var restVariable = null, 
-                restInterval = 1000,
-                restSec = restLength;
-            var sets = [];
-            var x = 0;
-            function restFunc () {   
-            function restClock (val) {return val > 9 ? val : "0" + val;}
-
-            if (restVariable !== null) return;    
-            var restTimer = setInterval(function(){
-                $("#restSeconds").html(restClock(--restSec%60));
-                $("#restCounter").html(restClock(parseInt(restSec/restLength, 10)));
-                 if(restSec === 0){
-                    clearInterval(restTimer);
-                    $('#restSeconds').html('--');  
-                    clock();
-                    for ( x = 0; sets < numberOfSets; x++){
-                        sets.push(x);
-                        console.log(x);
-                    } // for function 
-                }
-            }, restInterval);
-               
-            console.log(restSec);
-            console.log(sets);    
-            };
+            var looperArray = [];  
+            for (var i = 0; i < numberOfSets; i++){
+                looperArray[i] = looperFunction(i);
+//                e.preventDefault();
+                console.log(i);
+            }; //for loop
+            console.log(looperArray);
+            
        });// start tabata click event
+        
+        function looperFunction() { 
+        
+            $('#stop').click(function(){
+                restFunc();
+            });  
 
+            //start timer
+            var timer = null, 
+                interval = 1000,
+                sec = setLength;
+
+                    if (timer !== null) return;
+                    function clock (val) {return val > 9 ? val : "0" + val;}
+                    timer = setInterval(function(){
+                        $("#seconds").html(clock(--sec%60));
+//                        $("#counter").html(looperArray[i]);  
+                         if(sec === 0){
+                            clearInterval(timer);
+                            restFunc();
+                            $('#seconds').html('--');
+                         } else {
+                            $('#stop').click(function(){
+                                clearInterval(timer);
+                                $('#seconds').html('--');
+                            });   
+                         } //if
+                     }, interval);
+                    //timer
+
+                //rest timer
+                var restVariable = null, 
+                    restInterval = 1000,
+                    restSec = restLength;
+
+                function restFunc () {   
+                    function restClock (val) {return val > 9 ? val : "0" + val;}
+
+                    if (restVariable !== null) return;    
+                    var restTimer = setInterval(function(){
+                        $("#restSeconds").html(restClock(--restSec%60));
+                        $("#restCounter").html(restClock(parseInt(restSec/restLength, 10)));
+                         if(restSec === 0){
+                            clearInterval(restTimer);
+                            $('#restSeconds').html('--');  
+                         } //if
+                    }, restInterval);
+                    clock();
+                    
+                    };
+            return looperFunction;
+                }; // looperFunction  
+        
+        
     }); //submit event
 }); //document.ready 
 
