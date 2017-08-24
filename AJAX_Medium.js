e3efd768be275de49ac2db3a6bba1600bd8821fd
Shane <sdarcy@redventures.com>
2017-08-23 21:43:21 -0400

    weather ap Bonus complete

--- AJAX_Medium.js
+++ AJAX_Medium.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $('#getWeather').click(function(e){
         e.preventDefault();
+        var city = $('#city').val();
+        var URL = "http://weathers.co/api.php?city=" + city;
                     
-        $.getJSON('http://weathers.co/api.php?city=charlotte', function(rsp){
+        $.getJSON(URL, function(rsp){
             console.log(rsp);
             
              // Set the value for Fahrenheit
@@ -10,7 +12,7 @@ $(document).ready(function(){
             var fahrenheit = Math.round(celsius * 9 / 5 + 32);
             
             var forecast = $('<p>');
-            forecast.html(rsp.data['location'] +  "'s current temperature is " + fahrenheit + "&#8457; and currently " + rsp.data['skytext'] +".");
+            forecast.html(rsp.data['location'] +  "'s current temperature is " + fahrenheit + "&#8457; with " + rsp.data['skytext'] +".");
             
           $('#weatherResponse').append(forecast);
         
